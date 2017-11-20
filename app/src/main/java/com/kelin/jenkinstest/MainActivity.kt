9f20a2cd9abece4a788c4388c7ad350450262e83
kelin <yankelin@aliyun.com>
2017-11-20 13:32:37 +0800

    commit change to test

--- app/src/main/java/com/kelin/jenkinstest/MainActivity.kt
+++ app/src/main/java/com/kelin/jenkinstest/MainActivity.kt
@@ -2,11 +2,14 @@ package com.kelin.jenkinstest
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("hshshhsh","dfdfdfdfdf")
+
     }
 }
