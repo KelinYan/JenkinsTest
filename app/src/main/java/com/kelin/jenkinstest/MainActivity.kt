c8412606a01abc07948bc1d035d858f4e14f0ce2
kelin <yankelin@aliyun.com>
2020-04-16 10:45:44 +0800

    update

--- app/src/main/java/com/kelin/jenkinstest/MainActivity.kt
+++ app/src/main/java/com/kelin/jenkinstest/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 
+
+//hahahahahahahahhahaha
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
